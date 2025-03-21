12776bff051db2b8f8dd59a00b0d7280b19cfa30
Jim Wiggers <s1157305@student.hsleiden.nl>
2025-03-21 09:42:54 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {WebshopHomeComponent} from './webshop-home/webshop-home.component';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,6 @@ import {WebshopHomeComponent} from './webshop-home/webshop-home.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'lutetiaWebshop';
+  title = 'INFIRFS_s1157305_Frontend';
+  protected readonly environment = environment;
 }

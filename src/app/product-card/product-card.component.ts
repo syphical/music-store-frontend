784a77bc5a1bc8ef441ebf214c0d6402ad9a30db
Jim Wiggers <s1157305@student.hsleiden.nl>
2025-03-25 22:02:06 +0100

    Cleaned up product-card

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,7 +1,7 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CurrencyPipe} from '@angular/common';
 import {Product} from '../models/Product';
-import {Router, RouterLink} from '@angular/router';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
@@ -15,13 +15,8 @@ import {Router, RouterLink} from '@angular/router';
 export class ProductCardComponent {
   @Input({required: true}) public product!: Product;
   @Output() productAdded = new EventEmitter<Product>();
-  private router = inject(Router);
 
   addToCart() {
     this.productAdded.emit(this.product);
-  }
-
-  openDetails(product: Product) {
-    this.router.navigate(['/products', product.id]);
   }
 }

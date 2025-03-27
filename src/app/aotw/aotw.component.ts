2641c5bbcd054059478af8c30fa5b7451a07bef3
Jim Wiggers <s1157305@student.hsleiden.nl>
2025-03-27 23:05:17 +0100

    Album of the week component added

---
+++ src/app/aotw/aotw.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aotw',
+  imports: [],
+  templateUrl: './aotw.component.html',
+  styleUrl: './aotw.component.scss'
+})
+export class AotwComponent {
+
+}
--- src/app/webshop-home/webshop-home.component.ts
+++ src/app/webshop-home/webshop-home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {WebshopHeaderComponent} from '../webshop-header/webshop-header.component';
 import {ProductCardComponent} from '../product-card/product-card.component';
 import {WebshopProductListComponent} from '../webshop-product-list/webshop-product-list.component';
+import {AotwComponent} from '../aotw/aotw.component';
 
 @Component({
   selector: 'app-webshop-home',
-  imports: [WebshopProductListComponent],
+  imports: [WebshopProductListComponent, AotwComponent],
   templateUrl: './webshop-home.component.html',
   styleUrl: './webshop-home.component.scss'
 })

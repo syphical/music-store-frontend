0c207900cbedc4998e244b4afcbf72b5cd3beb45
Jim Wiggers <s1157305@student.hsleiden.nl>
2025-04-07 12:41:13 +0200

    Seperated front/backend

--- backend/src/main/java/com/lutetia/lutetiawebshop/dao/ArtistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lutetia.lutetiawebshop.dao;
-
-import com.lutetia.lutetiawebshop.models.Artist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArtistRepository extends JpaRepository<Artist, Long> {
-}
--- backend/src/main/java/com/lutetia/lutetiawebshop/dao/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lutetia.lutetiawebshop.dao;
-
-import com.lutetia.lutetiawebshop.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
